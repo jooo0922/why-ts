e9dfab214d9ef020555cb0afb1d050c973034876
jooo0922 <2014102458@khu.ac.kr>
2022-03-13 00:47:16 +0900

    add: Compare '.js' to '.ts'

---
+++ app.js
@@ -0,0 +1,54 @@
+// api url
+// 데이터를 가져올 서버의 url 저장해둔 것.
+var url = "https://jsonplaceholder.typicode.com/users/1";
+
+// dom
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
+
+// user data
+var user = {};
+
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {});
+
+function startApp() {
+  // axios
+  // .get(url)
+  fetchUser()
+    .then(function (response) {
+      // console.log(response);
+      user = response.data;
+      // TODO: 이름, 이메일, 주소 표시하기
+
+      console.log(user);
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address.street;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+startApp();
